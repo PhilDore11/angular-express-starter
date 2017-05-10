94c86116c4a9dd8c952db7c6dabdb8a125f2729e
Philippe Dore <pdore@lixar.com>
2017-05-10 13:48:45 -0400

    Convert Epi Curve to being a bar graph

--- client/components/reports/report/epiCurve/epiCurve.controller.js
+++ client/components/reports/report/epiCurve/epiCurve.controller.js
@@ -7,14 +7,22 @@ angular.module('app.reports.report.epiCurve', [])
     var epiCurveData = {
       labels: [],
       datasets: [{
+        type: 'bar',
         label: 'Resident Cases',
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         data: []
       }, {
+        type: 'bar',
         label: 'Staff Cases',
         backgroundColor: 'rgba(54, 162, 235, 0.2)',
         borderColor: 'rgba(54, 162, 235, 1)',
+        data: []
+      }, {
+        type: 'line',
+        label: 'Total Cases',
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        borderColor: 'rgba(100, 100, 100, 1)',
         data: []
       }]
     };
@@ -24,11 +32,12 @@ angular.module('app.reports.report.epiCurve', [])
       epiCurveData.labels.push($filter('date')(dataEntry.date));
       epiCurveData.datasets[0].data.push(dataEntry.residentCases);
       epiCurveData.datasets[1].data.push(dataEntry.staffCases);
+      epiCurveData.datasets[2].data.push(dataEntry.residentCases + dataEntry.staffCases);
     });
 
     var ctx = document.getElementById('epiCurve');
     var myChart = new Chart(ctx, {
-      type: 'line',
+      type: 'bar',
       data: epiCurveData,
       options: {
         responsive: true,
